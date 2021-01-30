74a41c5d5bcabdf5cc6cca412ce752be6faa207e
Ariel <ariel20006@walla.com>
2021-01-30 17:21:33 +0200

    feat(Navbar): added new NavBar, deleted old NavBar, button and DropDown menu, added pages Home, Products and Reports for demo and updated App

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import NavBar from './components/NavBar/NavBar'
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Navbar/Navbar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
-function App() {
-    return (
-        <div className='App'>
-            <Router>
-                <NavBar />
-            </Router>
-        </div>
-    )
+export default () => {
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
-
-export default App
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,60 +1,44 @@
-import React, {useState} from 'react'
-import {Button} from '../Button/Button'
-import {Link} from 'react-router-dom'
-import './NavBar.css'
-import DropDown from '../DropDown/DropDown'
+import React, { useState } from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+import './Navbar.css';
+import { IconContext } from 'react-icons';
 
-function NavBar() {
-    const [click, setClick] = useState(false)
-    const [dropdown, setDropdown] = useState(false)
+export default () => {
+  const [sidebar, setSidebar] = useState(false);
 
-    const handleClick = () => setClick(!click)
-    const closeMobileMenu = () => setClick(false)
+  const showSidebar = () => setSidebar(!sidebar);
 
-    const onMouseEnter = () => {
-        window.innerWidth < 960 ? setDropdown(false) : setDropdown(true)
-    }
-
-    const onMouseLeave = () => {
-        window.innerWidth < 960 ? setDropdown(false) : setDropdown(false)
-    }
-
-    return (
-        <div>
-            <nav className='navbar'>
-                <Link to='/' className='navbar-logo'>
-                    EPIC
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                            Services <i className='fas fa-caret-down' />
-                        </Link>
-                        {dropdown && <DropDown />}
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
-                            Contact Us
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                            Sign Up
-                        </Link>
-                    </li>
-                </ul>
-                <Button />
-            </nav>
+  return (
+    <>
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <div className='navbar'>
+          <Link to='#' className='menu-bars'>
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
         </div>
-    )
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
 }
-
-export default NavBar
